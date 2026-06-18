f23423b5037297fbcaeac1a20abff7daa582c024
admin <admin>
2026-06-18 15:37:40 +0200

    Edited file 'custom-recipes/get-instance-data/recipe.py'

--- custom-recipes/get-instance-data/recipe.py
+++ custom-recipes/get-instance-data/recipe.py
@@ -1,61 +1,12 @@
-# Code for custom code recipe get-instance-data (imported from a Python recipe)
-
-# To finish creating your custom recipe from your original PySpark recipe, you need to:
-#  - Declare the input and output roles in recipe.json
-#  - Replace the dataset names by roles access in your code
-#  - Declare, if any, the params of your custom recipe in recipe.json
-#  - Replace the hardcoded params values by acccess to the configuration map
-
-# See sample code below for how to do that.
-# The code of your original recipe is included afterwards for convenience.
-# Please also see the "recipe.json" file for more information.
-
-# import the classes for accessing DSS objects from the recipe
-import dataiku
-# Import the helpers for custom recipes
-from dataiku.customrecipe import get_input_names_for_role
-from dataiku.customrecipe import get_output_names_for_role
-from dataiku.customrecipe import get_recipe_config
-
-# Inputs and outputs are defined by roles. In the recipe's I/O tab, the user can associate one
-# or more dataset to each input and output role.
-# Roles need to be defined in recipe.json, in the inputRoles and outputRoles fields.
-
-# To  retrieve the datasets of an input role named 'input_A' as an array of dataset names:
-input_A_names = get_input_names_for_role('input_A_role')
-# The dataset objects themselves can then be created like this:
-input_A_datasets = [dataiku.Dataset(name) for name in input_A_names]
-
-# For outputs, the process is the same:
-output_A_names = get_output_names_for_role('instance_data')
-output_A_datasets = [dataiku.Dataset(name) for name in output_A_names]
-
-
-# The configuration consists of the parameters set up by the user in the recipe Settings tab.
-
-# Parameters must be added to the recipe.json file so that DSS can prompt the user for values in
-# the Settings tab of the recipe. The field "params" holds a list of all the params for wich the
-# user will be prompted for values.
-
-# The configuration is simply a map of parameters, and retrieving the value of one of them is simply:
-#my_variable = get_recipe_config()['parameter_name']
-
-# For optional parameters, you should provide a default value in case the parameter is not present:
-#my_variable = get_recipe_config().get('parameter_name', None)
-
-# Note about typing:
-# The configuration of the recipe is passed through a JSON object
-# As such, INT parameters of the recipe are received in the get_recipe_config() dict as a Python float.
-# If you absolutely require a Python int, use int(get_recipe_config()["my_int_param"])
-
-
-#############################
-# Your original recipe
-#############################
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Code for custom code recipe get-instance-data
+#
+# This recipe has no inputRoles: it doesn't act on a user-supplied dataset,
+# it queries the whole DSS instance through dataiku.api_client(). Only the
+# output side of the input/output-role pattern applies here, via
+# get_output_names_for_role("main_output").
 # -*- coding: utf-8 -*-
 import dataiku
+from dataiku.customrecipe import get_output_names_for_role
 import pandas as pd
 from datetime import datetime, timezone
 
@@ -295,13 +246,10 @@ for project_key in client.list_project_keys():
         "govern_tracked": get_govern_tracked(project),
     })
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 instance_data_df = pd.DataFrame(rows)
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-instance_data_df
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Write recipe outputs
-instance_data = dataiku.Dataset("instance_data")
+# Write recipe outputs: write to whatever dataset the user connected to the
+# "main_output" role in the recipe's I/O tab (Code 6 pattern, output side).
+output_names = get_output_names_for_role("main_output")
+instance_data = dataiku.Dataset(output_names[0])
 instance_data.write_with_schema(instance_data_df)
