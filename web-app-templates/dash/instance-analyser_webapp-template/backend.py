7adf2e708eeb1acdede44ff715f3c7e5701c6d3e
admin <admin>
2026-06-19 14:24:10 +0200

    Edited file 'web-app-templates/dash/instance-analyser_webapp-template/backend.py'

--- web-app-templates/dash/instance-analyser_webapp-template/backend.py
+++ web-app-templates/dash/instance-analyser_webapp-template/backend.py
@@ -1,2 +1,18 @@
-# This file is the actual code for the backend of Dash app instance-analyser_webapp-template
+from dataiku.customwebapp import get_webapp_config
+import dataiku
 
+# 1. Fetch the user's complete UI selections as a dictionary
+config = get_webapp_config()
+
+# 2. Extract values using the precise 'name' keys from webapp.json
+dataset_name = config.get("input_dataset")
+folder_id = config.get("target_folder")
+group_column = config.get("dimension_col")
+numerical_metrics = config.get("metric_cols", [])
+should_sample = config.get("enable_sampling", False)
+
+# 3. Instantiate Dataiku objects dynamically
+dataset = dataiku.Dataset(dataset_name)
+df = dataset.get_dataframe()
+
+# Now 'df' is ready to be handled by native Dash code (dcc, html, callback loops)!
